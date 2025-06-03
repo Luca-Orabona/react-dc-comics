acad7f511ce08439779e62ec5aceca9cc5858126
Luca-Orabona <lucaorabona94@gmail.com>
2025-06-03 15:38:37 +0200

    feat: main added

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,6 +1,54 @@
+import style from "./AppMain.module.css";
+
 function AppMain() {
     return (
-        <main></main>
+        <main className={style.main}>
+            <div className="container">
+                <p>--Content goes here--</p>
+            </div>
+
+            <section className={style.sectionNav}>
+                <div className="container">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <img src="/buy-comics-digital-comics.png" alt="" />
+                                <span>DIGITAL COMICS</span>
+                            </a>
+                        </li>
+
+                         <li>
+                            <a href="#">
+                                <img src="/buy-comics-merchandise.png" alt="" />
+                                <span>DC MERCHANDISE</span>
+                            </a>
+                        </li>
+
+                         <li>
+                            <a href="#">
+                                <img src="/buy-comics-subscriptions.png" alt="" />
+                                <span>SUBSCRIPTION</span>
+                            </a>
+                        </li>
+
+                         <li>
+                            <a href="#">
+                                <img src="/buy-comics-shop-locator.png" alt="" />
+                                <span>COMIC SHOP LOCATOR</span>
+                            </a>
+                        </li>
+
+                         <li>
+                            <a href="#">
+                                <img src="/buy-dc-power-visa.svg" alt="" />
+                                <span>DC POWER VISA</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
+        </main>
     )
 }
 
