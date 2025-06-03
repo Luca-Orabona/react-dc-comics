d8b3ef969aa923c8dcb2c2af0307dd57ceb5d3bc
Luca-Orabona <lucaorabona94@gmail.com>
2025-06-03 15:10:26 +0200

    feat: footer added

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,7 +1,85 @@
+import style from "./AppFooter.module.css";
+
 function AppFooter() {
     return (
-        
-        <footer></footer>
+
+        <footer className={style.footer}>
+            <div className="container">
+                {/* footer top */}
+                <div className={`${style.footerTop} ${style.grid}`}>
+                    <nav className={`${style.footerNav} ${style.grid}`}>
+                        <div>
+                            <ul>
+                                <li><h2>dc comics</h2></li>
+                                <li><a href="#">characters</a></li>
+                                <li><a href="#">comics</a></li>
+                                <li><a href="#">movies</a></li>
+                                <li><a href="#">tv</a></li>
+                                <li><a href="#">games</a></li>
+                                <li><a href="#">videos</a></li>
+                                <li><a href="#">news</a></li>
+                            </ul>
+
+                            <ul>
+                                <li><h2>shop</h2></li>
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+
+                        <ul>
+                            <li><h2>dc</h2></li>
+                            <li><a href="#">characters</a></li>
+                            <li><a href="#">comics</a></li>
+                            <li><a href="#">movies</a></li>
+                            <li><a href="#">tv</a></li>
+                            <li><a href="#">games</a></li>
+                            <li><a href="#">videos</a></li>
+                            <li><a href="#">news</a></li>
+                        </ul>
+
+                        <ul>
+                            <li><h2>sites</h2></li>
+                            <li><a href="#">characters</a></li>
+                            <li><a href="#">comics</a></li>
+                            <li><a href="#">movies</a></li>
+                            <li><a href="#">tv</a></li>
+                            <li><a href="#">games</a></li>
+                            <li><a href="#">videos</a></li>
+                            <li><a href="#">news</a></li>
+                        </ul>
+                    </nav>
+
+                    <div>
+                        <img src="/dc-logo-bg.png" alt="logo" />
+                    </div>
+                </div>
+
+            </div>
+
+            {/* footer bottom */}
+            <div className={style.footerBottom}>
+                <div className="container">
+                    
+                    <div className={style.row}>
+
+                        <a href="#" className={style.btn}>
+                            SIGN-UP NOW!
+                        </a>
+
+                        <ul className={style.icons}>
+                            <li className={style.colorBlue}>FOLLOW USE</li>
+                            <li><a href="#"><img src="/footer-facebook.png" alt="logo facebook" /></a></li>
+                            <li><a href="#"><img src="/footer-twitter.png" alt="logo twitter" /></a></li>
+                            <li><a href="#"><img src="/footer-youtube.png" alt="logo youtube" /></a></li>
+                            <li><a href="#"><img src="/footer-pinterest.png" alt="logo pinterest" /></a></li>
+                            <li><a href="#"><img src="/footer-periscope.png" alt="logo periscope" /></a></li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,20 +1,26 @@
+import style from "./AppHeader.module.css";
+
 function AppHeader() {
     return (
-        <header className="header">
+        <header>
+            <div className="container">
+                <nav className={style.row}>
+                    <img src="/dc-logo.png" alt="logo dc-comics" />
+                    <ul>
+                        <li><a href="#">characters</a></li>
+                        <li><a href="#">comics</a></li>
+                        <li><a href="#">movies</a></li>
+                        <li><a href="#">tv</a></li>
+                        <li><a href="#">games</a></li>
+                        <li><a href="#">collectibles</a></li>
+                        <li><a href="#">videos</a></li>
+                        <li><a href="#">fans</a></li>
+                        <li><a href="#">news</a></li>
+                        <li><a href="#">shop</a></li>
+                    </ul>
+                </nav>
 
-            <img src="/dc-logo.png" alt="logo" />
-            <ul>
-                <li><a href="#">characters</a></li>
-                <li><a href="#">comics</a></li>
-                <li><a href="#">movies</a></li>
-                <li><a href="#">tv</a></li>
-                <li><a href="#">games</a></li>
-                <li><a href="#">collectibles</a></li>
-                <li><a href="#">videos</a></li>
-                <li><a href="#">fans</a></li>
-                <li><a href="#">news</a></li>
-                <li><a href="#">shop</a></li>
-            </ul>
+            </div>
 
         </header>
     )
